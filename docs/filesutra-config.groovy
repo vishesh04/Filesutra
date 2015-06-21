7d5e75f46950c08d7ef328b79a6951c8dce1359e
vishesh <vishesh.singhal@eltropy.com>
2015-06-21 23:57:28 +0530

    Moved config to external file

---
+++ docs/filesutra-config.groovy
@@ -0,0 +1,38 @@
+dataSource {
+    username = '*****'
+    password = '*****'
+}
+
+auth {
+    google {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+
+    dropbox {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+
+    box {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+
+    onedrive {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+}
+
+grails {
+    plugin {
+        cookiesession {
+            secret = "*****"
+        }
+    }
+}
---
+++ grails-app/conf/DataSource.groovy
@@ -0,0 +1,55 @@
+dataSource {
+    pooled = true
+    driverClassName = "org.postgresql.Driver"
+    dialect = org.hibernate.dialect.PostgreSQL9Dialect
+    url = "jdbc:postgresql://localhost:5432/cfilesdb"
+    /* Moved to external config file
+    username = '*********'
+    password = '*********'
+    */
+
+    dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
+}
+
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+    reload = false
+    singleSession = true // configure OSIV singleSession mode
+    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
+}
+
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            //dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
+            //url = "jdbc:postgresql://localhost:5432/eltropydb"
+        }
+    }
+    test {
+        dataSource {
+            //dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
+            //url = "jdbc:postgresql://localhost:5432/eltropydb"
+        }
+    }
+    production {
+        dataSource {
+            pooled = true
+            properties {
+                maxActive = -1
+                minEvictableIdleTimeMillis = 1800000
+                timeBetweenEvictionRunsMillis = 1800000
+                numTestsPerEvictionRun = 3
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = true
+                validationQuery = "SELECT 1"
+            }
+
+            //dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
+            //url = "jdbc:postgresql://localhost:5432/eltropydb"
+        }
+    }
+}
