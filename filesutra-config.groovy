6a8a22be11d167a6571e6c94e2c621910dd77220
karthik <ykarthikreddy18>
2016-08-08 15:27:05 +0530

    REFS:#872 Additional config file for fileops

---
+++ filesutra-config.groovy
@@ -0,0 +1,55 @@
+dataSource {
+    username = '*****'
+    password = '*****'
+}
+
+auth {
+    google {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+
+    facebook {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+
+    flickr {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+    picasa {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+
+    dropbox {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+
+    box {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+
+    onedrive {
+        CLIENT_ID = "*****"
+        CLIENT_SECRET = "*****"
+        REDIRECT_URI = "*****"
+    }
+}
+
+grails {
+    plugin {
+        cookiesession {
+            secret = "*****"
+        }
+    }
+}
